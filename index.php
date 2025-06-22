2a772a0924bbea4b3320fe1e1f0ddb78845bd33b
saba pailodze <147437662+sabaPailodze@users.noreply.github.com>
2025-06-23 03:05:52 +0400

    Implement secure parameterized query to prevent SQL injection

--- index.php
+++ index.php
@@ -4,10 +4,12 @@ require 'fns.php';
 require 'Database.php';
 
 $config = require('config.php');
-
 $db = new Database($config['database']);
-$posts = $db -> query('select * from posts')->fetchAll(PDO::FETCH_ASSOC);
 
-dd($posts);
+$id = $_GET['id'];
+$query = 'select * from posts where id = ?';
 
+$posts = $db -> query($query,[$id])->fetch();
+
+dd($posts)
 ?>
