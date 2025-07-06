8263afb830a88520fbd9b21f54e03df23eae83e9
saba pailodze <147437662+sabaPailodze@users.noreply.github.com>
2025-07-07 00:34:01 +0400

    Refactor Database queries with new helper methods

--- Database.php
+++ Database.php
@@ -2,6 +2,8 @@
 
 class Database{
     public $connection;
+    public $statement;
+
     public function __construct($config, $username = 'root', $password = '')
     {
         $dsn = 'mysql:' . http_build_query($config,'',';');
@@ -12,9 +14,27 @@ class Database{
     }
     public function query($query,$params = []){
 
-        $statement = $this->connection->prepare($query);
-        $statement -> execute($params);
-        return $statement;
+        $this->statement = $this->connection->prepare($query);
+        $this->statement -> execute($params);
+        return $this;
+    }
+    
+
+
+    public function find() {
+        return $this->statement->fetch();
+    }
+
+    public function findOrFail(){
+       $result =  $this->find();
+       if(!$result){
+        abort();
+       }
+       return $result;
+    }
+
+    public function get(){
+        return $this->statement->fetchAll();
     }
 }
 
