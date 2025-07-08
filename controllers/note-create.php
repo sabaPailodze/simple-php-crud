93dc715ed542c3daa4a059fae4b7c63bf2e0a23f
saba pailodze <147437662+sabaPailodze@users.noreply.github.com>
2025-07-08 04:13:43 +0400

    Add simple form validation to note creation

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -6,11 +6,28 @@ $db = new Database($config['database']);
 $heading = 'Create Note';
 
 
-if($_SERVER['REQUEST_METHOD']==='POST'){    
-    $db->query("INSERT INTO notes(body , user_id) VALUES(:body, :user_id)", [
-        'body' => $_POST['body'],
-        'user_id' => 1
-]);
+if($_SERVER['REQUEST_METHOD']==='POST'){   
+    
+    $errors = [];
+    $congrats = [];
+
+    if(strlen($_POST['body']) === 0){
+        $errors['body'] = 'A body is required';
+    }
+
+    if(strlen($_POST['body']) > 10){
+        $errors['body'] = 'the body can not be more than 10 chars';
+    }
+
+    if(empty($errors)){
+        $db->query("INSERT INTO notes(body , user_id) VALUES(:body, :user_id)", [
+            'body' => $_POST['body'],
+            'user_id' => 1
+        ]);
+
+        $congrats['body'] = 'Congrats your note sent succesfully';
+    }
+
 }
 
 require 'views/note-create.view.php';
--- views/note-create.view.php
+++ views/note-create.view.php
@@ -13,7 +13,10 @@
         <div class="col-span-full">
           <label for="body" class="block text-sm/6 font-medium text-gray-900">Body</label>
           <div class="mt-2">
-            <textarea name="body" id="body" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" placeholder="Here's an idea for a new note..."></textarea>
+            <textarea name="body" id="body" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" placeholder="Here's an idea for a new note..."  ><?= $_POST['body'] ?? '' ?></textarea>
+            <?php if(isset($errors['body'])) : ?>
+              <p class="text-red-500 text-xs mt-2"><?= $errors['body'] ?></p>
+            <?php endif; ?>
           </div>
         </div>
 
